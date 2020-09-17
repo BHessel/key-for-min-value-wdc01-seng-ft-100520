1ed23a61f0eafd5ca1ec144bf18e6ea4ed9788fb
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-17 01:24:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,12 @@
+require 'pry'
+
 def key_for_min_value(name_hash)
-  lowkey = name_hash + 1
-  #set a variable to compare to
+  lowkey = nil
+  lowval = nil
   name_hash.each do |key, value|
+    binding.pry
     if value < lowkey
+    
       lowkey = value + key
       #in this block, if the value is < the lowkey variable, lowkey gets reassigned to current value
       #which allows me to isolate the key/value pair w/ lowest integer?
