31e927e419f6197ca139cccda066232a337f3cbe
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-17 01:11:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
   lowkey = name_hash + 1
   #set a variable to compare to
