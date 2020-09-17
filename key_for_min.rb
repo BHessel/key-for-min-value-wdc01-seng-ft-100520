7439e8d9dafb8a35a1c6e183ebf024eeecd61856
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-17 01:02:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowkey = name_hash + 1
+  #set a variable to compare to
+  name_hash.each do |key, value|
+    if value < lowkey
+      lowkey = value + key
+      #in this block, if the value is < the lowkey variable, lowkey gets reassigned to current value
+      #which allows me to isolate the key/value pair w/ lowest integer?
+    end
+    lowkey = name_hash[0][0]
+    lowkey
+end
 end
